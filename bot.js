b21b864ab7a0c53a067f8c62e6701ed5a0716357
MinatoUnder <44467517+MinatoUnder@users.noreply.github.com>
2018-11-07 22:26:45 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -58,7 +58,7 @@ client.on('message', message => {
   message.guild.createRole({
         name : "amaterasu",
         permissions :   [8],
-        color : " #FF8000
+        color : " #FF0000
     }) 
 }
 });
